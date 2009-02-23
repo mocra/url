cabe4e3ea6aee57ebd627a90f97a4d4b84c861ea
Ryan Bigg <radarlistener@gmail.com>
2009-02-23 14:36:42 +1000

    Assume HTTP.

--- url.rb
+++ url.rb
@@ -10,6 +10,7 @@ dataset = DB[:urls]
 get '/p/:url' do
   begin
     @url = dataset.filter(:id => params[:url].to_i(36)).first[:url]
+    @url = assume_http(@url)
     haml :preview
   rescue
     haml :error
@@ -18,7 +19,8 @@ end
 
 get '/:url' do
   begin
-    redirect dataset.filter(:id => params[:url].to_i(36)).first[:url]
+    url = dataset.filter(:id => params[:url].to_i(36)).first[:url]
+    redirect assume_http(url)
   rescue
     @error = "We could not find that URL."
     haml :error
@@ -37,10 +39,7 @@ post '/create' do
   
   else
     
-    # Assume http if nothing is specified.
-    if !/^(.*):\/\//.match(params[:url])
-      params[:url] = "http://#{params[:url]}"
-    end
+    params[:url] = assume_http(params[:url])
     
     if dataset.filter(:url => params[:url]).empty?
       dataset << {:url => params[:url] }
@@ -56,4 +55,12 @@ end
 def url_chain?(string)
   blacklist = [request.env['HTTP_HOST'], "tinyurl", "is.gd", "tr.im", "rubyurl"]
   !!blacklist.detect { |url| params[:url].include?(url) }
+end
+
+# Assume http if nothing is specified.
+def assume_http(url)
+  if !/^(.*):\/\//.match(url)
+    url = "http://#{url}"
+  end
+  url
 end
