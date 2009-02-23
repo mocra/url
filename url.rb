ca41396a2b6d0444f8633fe636822de639d4e043
Bodaniel Jeanes <me@bjeanes.com>
2009-02-23 12:42:27 +1000

    Had to split apart the views into separate files for passenger to use

--- url.rb
+++ url.rb
@@ -26,35 +26,4 @@ post '/create' do
   @id = url[:id][:id].to_s(36)
   haml :url
 end
-
-__END__
-
-@@ layout
-%html
-  %head
-    %link{ :rel => "stylesheet", :href => "style.css"}
-  %body
-    = yield
-  .footer
-    This site uses the
-    %a{ :href => "http://github.com/radar/url"} url
-    %a{ :href => "http://sinatrarb.com"} Sinatra
-    application built by
-    %a{ :href => "http://frozenplague.net"} Ryan Bigg
-    %b
-    
-@@ form
-%form{ :action => "create", :method => "post", :class => "url_form"}
-  %p
-    %label{ :for => "url" } URL goes here
-    %br/
-    %input{ :type => "text", :name => "url", :id => "url", :size => 45 }
-    %input{ :type => "submit", :name => "submit", :value => "URLize", :class => "button" }
-
-    
-
-@@ url
-== Your new URL is #{request.env["HTTP_ORIGIN"]}/#{@id}
-  
-
  
