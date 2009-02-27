cb94e6d32d45593dcf70d49f2062f876f550c3f9
Ryan Bigg <radarlistener@gmail.com>
2009-02-28 09:24:39 +1000

    Allow p in twitter URLs

--- public/application.js
+++ public/application.js
@@ -5,10 +5,10 @@ function setAsSeenOn(id, text, from_user, tweet_id) {
 function twitterCallback(data) {
   for (var i=0; i < data.results.length; i++) {
     var result = data.results[i];
-    findUrl = result.text.match(/u.mocra.com\/[^\s]+\b/g);
+    findUrl = result.text.match(/u.mocra.com\/[p\/]?[^\s]+\b/g);
     if (findUrl) {
       for (var j=0; j < findUrl.length; j++) {
-        var id = findUrl[j].match(/u.mocra.com\/([^\s]+)\b/)[1];
+        var id = findUrl[j].match(/u.mocra.com\/[p\/]?([^\s]+)\b/)[1];
         $('#as-seen-on-' + id).show();
         setAsSeenOn(id, result.text, result.from_user, result.id);
       }
