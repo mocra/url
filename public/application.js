f8f93b601834b6fa7f6b687190a9d0fb98629d05
Ryan Bigg <radarlistener@gmail.com>
2009-02-27 15:32:43 +1000

    Don't assume that users will always use u.mocra.com/permalink

--- public/application.js
+++ public/application.js
@@ -5,7 +5,10 @@ function setAsSeenOn(id, text, from_user, tweet_id) {
 function twitterCallback(data) {
   for (var i=0; i < data.results.length; i++) {
     var result = data.results[i];
-    var id = result.text.match(/u.mocra.com\/(.*?)\s/)[1];
+    find_url = result.text.match(/u.mocra.com\/(.*?)\s/)
+    if (find_url) {
+      var id = find_url[1];
+    }
     $('#as-seen-on-' + id).show();
     setAsSeenOn(id, result.text, result.from_user, result.id);
   }
