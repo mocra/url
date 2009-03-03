85bd983446fe6d12a902c87b2d9ed3303a2d47fc
Dr Nic Williams <drnicwilliams@gmail.com>
2009-03-03 21:32:30 +1000

    bookmarklet_link loads bookmarklet.js correctly

---
+++ public/javascripts/bookmarklet_link.js
@@ -0,0 +1,5 @@
+(function(){
+  script=document.createElement('script');
+  script.src='../../public/javascripts/bookmarklet.js';
+  document.getElementsByTagName('head')[0].appendChild(script);
+})();
