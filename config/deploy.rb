ad0a5c006cc10add17b8f962c2c9762b4ff7a798
Dr Nic Williams <drnicwilliams@gmail.com>
2009-03-04 12:26:33 +1000

    add database.yml cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :repository,  "git://github.com/mocra/#{application}.git"
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
-set :user, :radar
 set :ruby_vm_type,      :ree        # :ree, :mri
 set :web_server_type,   :apache     # :apache, :nginx
 set :app_server_type,   :passenger  # :passenger, :mongrel
@@ -25,8 +24,22 @@ role :web, domain
 # via the :deploy_to variable:
 set :deploy_to, "/opt/apps/#{application}"
 
+before 'deploy:cold', 'deploy:upload_database_yml'
+after 'deploy:symlink', 'deploy:create_symlinks'
+
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     top.deprec.app.restart
+  end
+  
+  desc "Uploads database.yml file to shared path"
+  task :upload_database_yml, :roles => :app do
+    put(File.read('config/database.yml'), "#{shared_path}/config/database.yml", :mode => 0644)
+  end
+  
+  desc "Symlinks database.yml file from shared folder"
+  task :create_symlinks, :roles => :app do
+    run "rm -f #{current_path}/config/database.yml"
+    run "ln -s #{shared_path}/config/database.yml #{current_path}/config/database.yml"
   end
 end
